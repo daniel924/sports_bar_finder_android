77c4964a892468a22e488c8f7b6ea5a35c661295
Daniel Ladenheim <ladenheim@gmail.com>
2014-12-26 17:07:23 -0500

    Made Bar into a fragment

--- app/src/main/java/com/sportsbarfinder/FindBarActivity.java
+++ app/src/main/java/com/sportsbarfinder/FindBarActivity.java
@@ -1,13 +1,16 @@
 package com.sportsbarfinder;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -43,6 +46,14 @@ public class FindBarActivity extends Activity {
                 ex.printStackTrace();
                 return null;
             }
+        }
+    }
+
+    public static class BarFragment extends Fragment {
+        @Override
+        public View onCreateView(
+                LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_bar, container, false);
         }
     }
 
